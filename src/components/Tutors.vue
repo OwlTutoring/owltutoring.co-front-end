743f8eb181a55f1ea30cbe8c9e1e1784a4d58060
KyungSun4 <john.han7@yahoo.com>
2018-06-27 17:55:23 -0500

    get tutor by id link

--- src/components/Tutors.vue
+++ src/components/Tutors.vue
@@ -15,15 +15,19 @@ export default {
   },
   data: function() {
     return {
-      tutors: []
+      tutors: [],
+      zipCode: this.$route.query.zipCode,
+      tutorID: this.$route.query.tutorID
     };
   },
   created: function() {
+    console.log(this.zipCode);
     document.title = "Tutors - Owl Tutoring";
     var _this = this;
+    console.log((this.tutorID ? "?tutorID=" + this.tutorID : ""));
     axios
       .get(
-        "https://z9yqr69kvh.execute-api.us-west-2.amazonaws.com/dev/getTutors"
+        "https://z9yqr69kvh.execute-api.us-west-2.amazonaws.com/dev/getTutors" + (this.tutorID ? "?tutorID=" + this.tutorID : "")
       )
       .then(function(response) {
         // JSON responses are automatically parsed.
