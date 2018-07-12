6c5f988cfb3f65c3b6ed263de4fa615ce13c4569
John <john.han7@yahoo.com>
2018-07-12 13:36:21 -0500

    requests with more info to tutors

--- src/components/TutorProfile.vue
+++ src/components/TutorProfile.vue
@@ -3,9 +3,9 @@
   <div id="tutor-profile">
     <h1>Tutor Profile</h1>
 
-    Thank you for your intrest in being an Owl Tutor. After you submit this profile someone at Owl Tutoring will review your qualifications and assuming you are qualified you will be contacted and your profile will be visible on our website. We will then work to connect you with students you are qualified to teach. In a tutor we are looking for high school and college students who are knowledgeable and able to teach a variety of topics at Elementary, Middle school or High School levels. This means you can teach whatever you want and are qualified to teach from algebra or world history to violin or photography. Tutoring sessions typical occur in public places like a library. Tutors can work as many hours that are available to them but can also can have fewer students if they are busy. If you have any questions please text <a href="sms://2248149291">(224) 814-9291</a>
+    Thank you for your intrest in being an Owl Tutor. After you submit this profile someone at Owl Tutoring will review your qualifications and assuming you are qualified you will be contacted and your profile will be visible on our website. We will then work to connect you with students you are qualified to teach. In a tutor we are looking for high school and college students who are knowledgeable and able to teach a variety of topics at Elementary, Middle school or High School levels. This means you can teach whatever you want and are qualified to teach from algebra or world history to violin or photography. Tutoring sessions typical occur in public places like a library. Tutors can work as many hours that are available to them but can also can have fewer students if they are busy. <br>If you have any questions please text <a href="sms://2248149291">(224) 814-9291</a><br>You can submit this profile before it is compleate to save your responses.<br>If your profile is already live, changes you make will be active imeadiatly.
     <h3 class="input-title">How did you find out about Owl Tutoring?</h3>
-    <input  type="text" placeholder="Jim Bob told me I would make a great Owl Tutor" class="line-input form-field" v-model="howSource" id="howSource">
+    <input type="text" placeholder="Jim Bob told me I would make a great Owl Tutor" class="line-input form-field" v-model="refferalSource" id="refferalSource">
     
     <h3 class="input-title">Refferal Code</h3>
     <p class="input-description">
@@ -50,7 +50,7 @@
     <input  type="text" placeholder="Vernon Hills" class="form-field" v-model="town" id="town">
     
     <h3 class="input-title">State</h3>
-    <select v-model="usState">
+    <select class="form-field" v-model="usState">
       <option v-for="state in states" :value="state">{{state}}</option>
     </select>
 
@@ -85,8 +85,8 @@
     <p class="input-description">
       Please also email a photo to be used for your public profile to <a href="mailto:general@owltutoring.co">general@owltutoring.co</a>    
     </p>
-
-    <button class="color-button" v-on:click="update()">Submit Profile</button>
+    <button class="plain-button" v-on:click="update(false)">Save Changes</button>
+    <button class="color-button" v-on:click="update(true)">Submit Profile</button>
   </div>
   <div class="after-margin"></div>
 </div>
@@ -107,16 +107,18 @@ export default {
   data: function() {
     return {
       states: ['AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT','VT','VI','VA','WA','WV','WI','WY'],
+      refferalSource: "",
+      refferalCode: "",
       bio: "",
+      subjectsDesc:"",
       grade: "",
+      school: "",
       town: "",
       usState: "",
       zipCode: "",
       experience: "",
       shortBio: "",
-      subjectsDesc:"",
       otherInfo:"",
-      school: "",
       selectedSubjects: {},
       subjectOptions: SubjectTypesStore.data.subjects,
     };
@@ -134,15 +136,20 @@ export default {
         // JSON responses are automatically parsed.
         console.log(response);
         MessageStore.methods.showMessage(response.data.message, false);
-        _this.bio = response.data.bio;
-        _this.grade = response.data.grade;
-        _this.town = response.data.town;
-        _this.usState = response.data.usState;
-        _this.zipCode = response.data.zipCode;
-        _this.experience = response.data.experience;
-        _this.shortBio = response.data.shortBio;
-        _this.subjectsDesc = response.data.subjectsDesc;
+        _this.refferalSource = response.data.refferalSource == "NULL" ? "" : response.data.refferalSource;
+        _this.refferalCode = response.data.refferalCode == "NULL" ? "" : response.data.refferalCode;
+        _this.bio = response.data.bio == "NULL" ? "" : response.data.bio;
+        _this.subjectsDesc = response.data.subjectsDesc == "NULL" ? "" : response.data.subjectsDesc;
         _this.selectedSubjects = _this.parseSubjectDataString(response.data.subjects);
+        _this.grade = response.data.grade == "0" ? "" : response.data.grade;
+        _this.school = response.data.school == "NULL" ? "" : response.data.school;
+        _this.town = response.data.town == "NULL" ? "" : response.data.town;
+        _this.usState = response.data.usState;
+        _this.zipCode = response.data.zipCode == "0" ? "" : response.data.zipCode;
+        _this.experience = response.data.experience == "NULL" ? "" : response.data.experience;
+        _this.shortBio = response.data.shortBio == "NULL" ? "" : response.data.shortBio;
+        _this.otherInfo = response.data.otherInfo == "NULL" ? "" : response.data.otherInfo;
+        
       })
       .catch(function(e) {
         console.log(e);
@@ -197,7 +204,7 @@ export default {
       console.log(res);
       return res;
     },
-    update: function() {
+    update: function(done) {
       var _this = this;
       console.log(this.selectedSubjects);
       this.getSubjectDataString();
@@ -206,16 +213,21 @@ export default {
         .post(
           "https://z9yqr69kvh.execute-api.us-west-2.amazonaws.com/dev/updateTutorProfile",
           {
-            bio: _this.bio,
-            grade: _this.grade,
-            town: _this.town,
-            zipCode: _this.zipCode,
-            usState: _this.usState,
-            experience: _this.experience,
-            shortBio: _this.shortBio,
-            subjectsDesc: _this.subjectsDesc,
+            refferalSource:_this.refferalSource,
+            refferalCode:_this.refferalCode,
+            bio:_this.bio,
+            subjectsDesc:_this.subjectsDesc,
+            grade:_this.grade,
+            school:_this.school,
+            town:_this.town,
+            usState:_this.usState,
+            zipCode:_this.zipCode,
+            experience:_this.experience,
+            shortBio:_this.shortBio,
+            otherInfo:_this.otherInfo,
             subjects: _this.getSubjectDataString(),
-            token: localStorage.getItem("token")
+            token: localStorage.getItem("token"),
+            done: done,
           }
         )
         .then(function(response) {
