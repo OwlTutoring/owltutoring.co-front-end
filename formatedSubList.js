f5cfdf8f455e0eb7247e247d29baba2c8ac61261
Han <jsh3@linux-a2.ews.illinois.edu>
2018-08-01 10:00:55 -0500

    ignore test dist

---
+++ formatedSubList.js
@@ -0,0 +1,874 @@
+var sublist = {
+subject: "",
+sublist: [
+{
+subject: "Mathematics",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Pre Algebra",
+},
+{
+subject: "Algebra 1",
+},
+{
+subject: "Algebra 2 - including trigonometry",
+},
+{
+subject: "Geometry",
+},
+{
+subject: "Pre Calculus",
+},
+{
+subject: "AP Calculus AB",
+},
+{
+subject: "AP Calculus BC",
+},
+{
+subject: "AP Statistics",
+},
+]
+},
+{
+subject: "Applied Mathematics",
+sublist: [
+{
+subject: "Computer Programming - Python & Java",
+},
+{
+subject: "AP Computer Science A",
+},
+{
+subject: "AP Computer Science Principles",
+},
+{
+subject: "Mobile App Development and Programming - is this a new class",
+},
+{
+subject: "PLTW Introduction to Engineering Design",
+},
+{
+subject: "PLTW Principles of Engineering Honors",
+},
+{
+subject: "PLTW Digital Electronics Honors",
+},
+{
+subject: "Architecture",
+},
+{
+subject: "PLTW Civil Engineering and Architecture Honors",
+},
+{
+subject: "PLTW Engineering Design and Development Honors",
+},
+{
+subject: "Web Development",
+},
+{
+subject: "Game Development",
+},
+]
+},
+{
+subject: "English/Language Arts",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "High School - including AP Language and Literature",
+},
+{
+subject: "Journalism",
+},
+{
+subject: "Acting",
+},
+{
+subject: "College Essay Writing",
+},
+]
+},
+{
+subject: "Social Studies",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "World History",
+},
+{
+subject: "AP World History",
+},
+{
+subject: "Geography",
+},
+{
+subject: "AP Human Geography",
+},
+{
+subject: "European History",
+},
+{
+subject: "AP United States History",
+},
+{
+subject: "Government",
+},
+{
+subject: "AP Government/AP Comparative Government",
+},
+{
+subject: "Psychology",
+},
+{
+subject: "AP Psychology",
+},
+{
+subject: "Sociology (Honors)",
+},
+{
+subject: "Philosophy (Honors)",
+},
+{
+subject: "Constitutional Law",
+},
+{
+subject: "AP Microeconomics",
+},
+{
+subject: "AP Macroeconomics",
+},
+{
+subject: "World Religions",
+},
+{
+subject: "Native World Cultures",
+},
+{
+subject: "Global Relations",
+},
+{
+subject: "Political Thought and Its Literature",
+},
+]
+},
+{
+subject: "Applied Science",
+sublist: [
+{
+subject: "Introduction to Business",
+},
+{
+subject: "Business Applications and Technology 1",
+},
+{
+subject: "Business Applications and Technology 2",
+},
+{
+subject: "Accounting 1",
+},
+{
+subject: "Accounting 2 Honors",
+},
+{
+subject: "Advanced Accounting Honors",
+},
+{
+subject: "Marketing",
+},
+{
+subject: "Entrepreneurship",
+},
+{
+subject: "Business Law",
+},
+{
+subject: "Investment Management",
+},
+{
+subject: "Personal Finance",
+},
+{
+subject: "Entrepreneurial Tactics Accelerated",
+},
+]
+},
+{
+subject: "Science",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Chemistry",
+},
+{
+subject: "AP Chemistry",
+},
+{
+subject: "Biology",
+},
+{
+subject: "AP Biology",
+},
+{
+subject: "Human Anatomy and Physiology",
+},
+{
+subject: "Physics",
+},
+{
+subject: "AP Physics 1",
+},
+{
+subject: "AP Physics 2",
+},
+{
+subject: "AP Physics C",
+},
+{
+subject: "AP Environmental Science",
+},
+{
+subject: "Earth Science",
+},
+]
+},
+{
+subject: "World Languages",
+sublist: [
+{
+subject: "Spanish",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Spanish 1",
+},
+{
+subject: "Spanish 2",
+},
+{
+subject: "Spanish 3",
+},
+{
+subject: "Spanish 4",
+},
+{
+subject: "AP Spanish 5 Language and Culture",
+},
+{
+subject: "AP Spanish 6 Literature and Culture",
+},
+]
+},
+{
+subject: "French",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "French 1",
+},
+{
+subject: "French 2",
+},
+{
+subject: "French 3",
+},
+{
+subject: "French 4",
+},
+{
+subject: "AP French 5",
+},
+]
+},
+{
+subject: "Chinese",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Chinese 1",
+},
+{
+subject: "Chinese 2",
+},
+{
+subject: "AP Chinese",
+},
+]
+},
+{
+subject: "German",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "German 1",
+},
+{
+subject: "German 2",
+},
+{
+subject: "German 3",
+},
+{
+subject: "AP German",
+},
+]
+},
+{
+subject: "Hebrew",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Hebrew 1",
+},
+{
+subject: "Hebrew 2",
+},
+{
+subject: "Hebrew 3",
+},
+{
+subject: "Hebrew 4",
+},
+]
+},
+{
+subject: "Latin",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Latin 1",
+},
+{
+subject: "Latin 2",
+},
+{
+subject: "Latin 3",
+},
+{
+subject: "AP Latin Vergil",
+},
+]
+},
+{
+subject: "Italian",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "High School/AP",
+},
+]
+},
+{
+subject: "Japanese",
+sublist: [
+{
+subject: "Elementary",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "High School/AP",
+},
+]
+},
+]
+},
+{
+subject: "Fine Arts - Music",
+sublist: [
+{
+subject: "Orchestra",
+sublist: [
+{
+subject: "Violin",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Cello",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Viola",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Bass",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Harp",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+]
+},
+{
+subject: "Band",
+sublist: [
+{
+subject: "Trumpet",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Trombone",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "French Horn",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Saxophone",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Oboe",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Bassoon",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Clarinet",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Bass Clarinet",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Baritone",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Tuba",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Flute",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Piccolo",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Percussion",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Piano",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+]
+},
+{
+subject: "Acoustic/Electric Guitar",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Bass Guitar",
+sublist: [
+{
+subject: "Beginner",
+},
+{
+subject: "Intermediate",
+},
+{
+subject: "Advanced",
+},
+]
+},
+{
+subject: "Chorus - vocal lessons",
+},
+{
+subject: "Music Theory - including AP",
+},
+{
+subject: "Indian Classical Carnatic Music",
+},
+]
+},
+{
+subject: "Fine Arts - Art",
+sublist: [
+{
+subject: "Drawing",
+},
+{
+subject: "Painting",
+},
+{
+subject: "Studio Art",
+},
+{
+subject: "Computer Art",
+},
+{
+subject: "Ceramics",
+},
+{
+subject: "Sculpture",
+},
+{
+subject: "Metals and Jewelry",
+},
+{
+subject: "AP Art: Drawing, 2D and 3D design",
+},
+{
+subject: "Photography - including AP Photography & Digital Design",
+},
+{
+subject: "AP Art History",
+},
+]
+},
+{
+subject: "Standardized Testing",
+sublist: [
+{
+subject: "ACT",
+sublist: [
+{
+subject: "Plan",
+},
+{
+subject: "Explore",
+},
+]
+},
+{
+subject: "PSAT/SAT",
+},
+{
+subject: "SAT Subject Tests",
+sublist: [
+{
+subject: "SAT Subject Test Literature",
+},
+{
+subject: "SAT Subject Test United States History",
+},
+{
+subject: "SAT Subject Test World History",
+},
+{
+subject: "SAT Subject Test in Mathematics Level 1",
+},
+{
+subject: "SAT Subject Test in Mathematics Level 2",
+},
+{
+subject: "SAT Subject Test in Biology E/M",
+},
+{
+subject: "SAT Subject Test in Chemistry",
+},
+{
+subject: "SAT Subject Test in Physics",
+},
+{
+subject: "SAT Subject Test in Physics",
+},
+{
+subject: "SAT Subject Test in Chinese with Listening",
+},
+{
+subject: "SAT Subject Test in French",
+},
+{
+subject: "SAT Subject Test in French with Listening",
+},
+{
+subject: "SAT Subject Test in German",
+},
+{
+subject: "SAT Subject Test in German with Listening",
+},
+{
+subject: "SAT Subject Test in Modern Hebrew",
+},
+{
+subject: "SAT Subject Test in Italian",
+},
+{
+subject: "SAT Subject Test in Japanese with Listening",
+},
+{
+subject: "SAT Subject Test in Korean with Listening",
+},
+{
+subject: "SAT Subject Test in Latin",
+},
+{
+subject: "SAT Subject Test in Spanish",
+},
+{
+subject: "SAT Subject Test in Spanish with Listening",
+},
+{
+subject: "",
+}
+]
+},
+]
+},
+]
+},
